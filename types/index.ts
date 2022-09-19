50fc297715fac0591301383389e53910fb4bd1f6
OriAfiasDev <ori@afias.net>
2022-09-19 21:17:15 +0300

    types

---
+++ types/index.ts
@@ -0,0 +1,27 @@
+export interface IProduct {
+  category: ICategory;
+  current_quantity: number;
+  order_quantity: number;
+  product_id: string;
+  product_name: string;
+  product_price: number;
+}
+
+interface ICategory {
+  category_id: string;
+  category_name: string;
+}
+
+interface IContact {
+  contact_id: string;
+  contact_name: string;
+  contact_phone: string;
+}
+
+export interface ICompany {
+  company_id: string;
+  company_name: string;
+  preferred_days: number[];
+  products: IProduct[];
+  contacts: IContact[];
+}
