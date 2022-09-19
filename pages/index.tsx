91a860745f5ffd7289d8f51f2d65cc84a3193eb6
OriAfiasDev <ori@afias.net>
2022-09-19 14:00:31 +0300

    test link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import { Link } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import NextLink from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -16,6 +18,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Link as={NextLink} color='teal.400' href='/companies/ביסקוטי'>ביסקוטי</Link>
 
         <p className={styles.description}>
           Get started by editing{' '}
