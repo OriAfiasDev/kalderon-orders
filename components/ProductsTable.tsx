ceb185391ab3c36c82a6935852deac1b5f41d092
OriAfiasDev <ori@afias.net>
2022-09-19 21:17:30 +0300

    dumb company page with table

---
+++ components/ProductsTable.tsx
@@ -0,0 +1,68 @@
+import { Button, Input, Table, Tbody, Td, Tfoot, Thead, Tr } from '@chakra-ui/react';
+import Router, { useRouter } from 'next/router';
+import React, { useCallback, useMemo } from 'react';
+import { IProduct } from '../types';
+
+interface ProductTableProps {
+  products: IProduct[];
+}
+
+export const ProductsTable: React.FC<ProductTableProps> = ({ products }) => {
+  const router = useRouter();
+  const [updatedProducts, setUpdatedProducts] = React.useState<IProduct[]>(products);
+  const canOrder = useMemo(() => updatedProducts.some(product => product.order_quantity > 0), [updatedProducts]);
+
+  const handleProductUpdate = (productId: string, field: keyof IProduct, value: any) => {
+    setUpdatedProducts(prev => prev.map(p => (p.product_id === productId ? { ...p, [field]: value } : p)));
+  };
+
+  const sendOrder = useCallback(() => {
+    const text = updatedProducts
+      .filter(p => p.order_quantity > 0)
+      .map(p => `${p.product_name} - ${p.order_quantity}`)
+      .join('/n');
+    router.push(`https://wa.me/972542533533?text=${text}`, '_blank');
+  }, [updatedProducts, router]);
+
+  return (
+    <> 
+    <Table dir='rtl' align='center' justifyContent='center'>
+      <Thead>
+        <Tr>
+          <Td>קטגוריה</Td>
+          <Td>שם מוצר</Td>
+          <Td>מלאי</Td>
+          <Td>הזמנה</Td>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {updatedProducts.map(product => (
+          <Tr key={product.product_id}>
+            <Td>{product.category.category_name}</Td>
+            <Td>{product.product_name}</Td>
+            <Td>
+              <Input
+                width='20'
+                type='number'
+                value={product.current_quantity}
+                onChange={e => handleProductUpdate(product.product_id, 'current_quantity', Number(e.target.value))}
+              />
+            </Td>
+            <Td>
+              <Input
+                width='20'
+                type='number'
+                value={product.order_quantity}
+                onChange={e => handleProductUpdate(product.product_id, 'order_quantity', Number(e.target.value))}
+              />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+    <Button color='teal.400' onClick={sendOrder} disabled={!canOrder} width='100%'>
+    שלח הזמנה
+  </Button>
+  </>
+  );
+};
--- pages/companies/[company_name].tsx
+++ pages/companies/[company_name].tsx
@@ -1,38 +1,40 @@
-import { convertNumToDay } from '../../utils/conversions';
 import { supabase } from '../../utils/supabaseClient';
 import { v4 as uuid } from 'uuid';
-
-type ICompany = {
-  company_id: string;
-  company_name: string;
-  preferred_days: number[];
-  products: any[];
-};
+import { Container, Heading, Highlight, Text } from '@chakra-ui/react';
+import { ICompany } from '../../types';
+import { ProductsTable } from '../../components/ProductsTable';
 
 const Company: React.FC<ICompany> = props => {
   console.log(props);
+  // console.log(uuid());
 
   return (
-    <div>
-      <h1>Company {props.company_name}</h1>
-      <p>Preferred days: {props.preferred_days.map(day => convertNumToDay(day) + ',')}</p>
-      <p>Products: {props.products.map(product => product.product_name + ',')}</p>
-    </div>
+    <Container>
+      <Heading textAlign='center' color='teal.500'>
+        {props.company_name}
+      </Heading>
+      {props.contacts.map(contact => (
+        <Text textAlign='center' fontSize='xl' key={contact.contact_id}>סוכן: {contact.contact_name} - {contact.contact_phone}</Text>
+      ))}
+
+      <ProductsTable products={props.products} />
+    </Container>
   );
 };
 
 export async function getServerSideProps(context: any) {
-  const preferred_days = await supabase
-    .from('companies')
-    .select('*')
-    .match({ company_name: context.params.company_name })
-    .then(res => (res.data?.length ? res.data[0].preferred_days : []));
+  const { company_name } = context.params;
 
-  const company = await supabase.from('companies').select('*').match({ company_name: context.params.company_name }).single();
-  const products = await supabase.from('products').select('*').match({ company_id: company.data.company_id });
+  const data = await supabase
+    .from('companies')
+    .select(
+      `*,products: products!company_id(product_id,product_name,product_price,order_quantity,current_quantity,category: category_id(*)),contacts: contacts!company_id(contact_id, contact_name, contact_phone)`
+    )
+    .match({ company_name })
+    .single();
 
   return {
-    props: { ...company.data, products: products.data },
+    props: data.data,
   };
 }
 
