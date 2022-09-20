a583af65a910cd948e6205de421c1cda6092425b
OriAfiasDev <ori@afias.net>
2022-09-20 09:11:35 +0300

    add product wip

---
+++ components/AddProduct.tsx
@@ -0,0 +1,48 @@
+import { Button, Container, Heading, Input, Select } from '@chakra-ui/react';
+import { useCallback, useState } from 'react';
+import { IProduct, ICategory } from '../types';
+import { v4 as uuid } from 'uuid';
+import { supabase } from '../utils/supabaseClient';
+
+interface AddProductProps {
+  company_id: string;
+  categories: ICategory[];
+}
+
+export const AddProduct: React.FC<AddProductProps> = ({ company_id, categories }) => {
+  const [productName, setProductName] = useState<string>('');
+  const [productCategoryId, setProductCategoryId] = useState<string>(categories[0].category_id);
+  const [productPrice, setProductPrice] = useState<number>(0);
+
+  console.log(categories);
+
+  const handleAdd = useCallback(async () => {
+    const product = {
+      product_id: uuid(),
+      product_name: productName,
+      category_id: productCategoryId,
+      current_quantity: 0,
+      order_quantity: 0,
+      product_price: productPrice,
+      company_id: company_id,
+    };
+
+    await supabase.from('products').insert(product);
+  }, [productCategoryId, productName, productPrice, company_id]);
+
+  return (
+    <Container mt='10' dir='rtl'>
+      <Heading fontSize='larger'>הוסף פריט</Heading>
+      <Input placeholder='שם מוצר' value={productName} onChange={e => setProductName(e.target.value)} />
+      <Input placeholder='מחיר' value={productPrice} onChange={e => setProductPrice(Number(e.target.value))} />
+      <Select placeholder='בחר קטגוריה' value={productCategoryId} onChange={e => setProductCategoryId(e.target.value)}>
+        {categories.map(c => (
+          <option key={c.category_id} value={c.category_id}>
+            {c.category_name}
+          </option>
+        ))}
+      </Select>
+      <Button onClick={handleAdd}>הוסף מוצר</Button>
+    </Container>
+  );
+};
