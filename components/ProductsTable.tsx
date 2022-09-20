10f5dbcfcf82ac8d93a92e5068b523ce9a62208a
OriAfiasDev <ori@afias.net>
2022-09-20 09:11:25 +0300

    whatsapp enter

--- components/ProductsTable.tsx
+++ components/ProductsTable.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Table, Tbody, Td, Tfoot, Thead, Tr } from '@chakra-ui/react';
+import { Button, Center, Input, Table, Tbody, Td, Tfoot, Thead, Tr } from '@chakra-ui/react';
 import Router, { useRouter } from 'next/router';
 import React, { useCallback, useMemo } from 'react';
 import { IProduct } from '../types';
@@ -20,49 +20,51 @@ export const ProductsTable: React.FC<ProductTableProps> = ({ products }) => {
     const text = updatedProducts
       .filter(p => p.order_quantity > 0)
       .map(p => `${p.product_name} - ${p.order_quantity}`)
-      .join('/n');
+      .join('%0a');
     router.push(`https://wa.me/972542533533?text=${text}`, '_blank');
   }, [updatedProducts, router]);
 
   return (
-    <> 
-    <Table dir='rtl' align='center' justifyContent='center'>
-      <Thead>
-        <Tr>
-          <Td>קטגוריה</Td>
-          <Td>שם מוצר</Td>
-          <Td>מלאי</Td>
-          <Td>הזמנה</Td>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {updatedProducts.map(product => (
-          <Tr key={product.product_id}>
-            <Td>{product.category.category_name}</Td>
-            <Td>{product.product_name}</Td>
-            <Td>
-              <Input
-                width='20'
-                type='number'
-                value={product.current_quantity}
-                onChange={e => handleProductUpdate(product.product_id, 'current_quantity', Number(e.target.value))}
-              />
-            </Td>
-            <Td>
-              <Input
-                width='20'
-                type='number'
-                value={product.order_quantity}
-                onChange={e => handleProductUpdate(product.product_id, 'order_quantity', Number(e.target.value))}
-              />
-            </Td>
+    <>
+      <Table dir='rtl' align='center' justifyContent='center'>
+        <Thead>
+          <Tr>
+            <Td>קטגוריה</Td>
+            <Td>שם מוצר</Td>
+            <Td>מלאי</Td>
+            <Td>הזמנה</Td>
           </Tr>
-        ))}
-      </Tbody>
-    </Table>
-    <Button color='teal.400' onClick={sendOrder} disabled={!canOrder} width='100%'>
-    שלח הזמנה
-  </Button>
-  </>
+        </Thead>
+        <Tbody>
+          {updatedProducts.map(product => (
+            <Tr key={product.product_id}>
+              <Td>{product.category.category_name}</Td>
+              <Td>{product.product_name}</Td>
+              <Td>
+                <Input
+                  width='20'
+                  type='number'
+                  value={product.current_quantity}
+                  onChange={e => handleProductUpdate(product.product_id, 'current_quantity', Number(e.target.value))}
+                />
+              </Td>
+              <Td>
+                <Input
+                  width='20'
+                  type='number'
+                  value={product.order_quantity}
+                  onChange={e => handleProductUpdate(product.product_id, 'order_quantity', Number(e.target.value))}
+                />
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+      <Center>
+        <Button color='teal.400' onClick={sendOrder} disabled={!canOrder}>
+          שלח הזמנה
+        </Button>
+      </Center>
+    </>
   );
 };
