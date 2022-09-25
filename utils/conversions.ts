28491a86269235998f8c1227d869719534fa6c50
OriAfiasDev <ori@afias.net>
2022-09-25 10:32:22 +0300

    removed all conversion method

--- utils/conversions.ts
+++ utils/conversions.ts
@@ -1,24 +1,3 @@
-export const convertNumToDay1 = (num: number) => {
-  switch (num) {
-    case 0:
-      return 'ראשון';
-    case 1:
-      return 'שני';
-    case 2:
-      return 'שלישי';
-    case 3:
-      return 'רביעי';
-    case 4:
-      return 'חמישי';
-    case 5:
-      return 'שישי';
-    case 6:
-      return 'שבת';
-    default:
-      return 'Invalid day';
-  }
-};
-
 export const convertNumToDay = (num: number, type: 'short' | 'long' = 'long') => {
   const days = { 
     short: ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ש'],
