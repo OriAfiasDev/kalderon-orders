c10f522fffda8bfb8bb21013e7823a8092b697ce
OriAfiasDev <ori@afias.net>
2022-09-25 10:22:09 +0300

    company name endpoint

--- pages/api/companies/[company_name].ts
+++ pages/api/companies/[company_name].ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { ICompany } from '@types';
 import { supabase } from '@utils/supabaseClient';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -6,7 +5,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ICompany>) {
   const { company_name } = req.query;
 
-  const data = await supabase
+  const { data, status } = await supabase
     .from('companies')
     .select(
       `*,products: products!company_id(product_id,product_name,product_price,order_quantity,current_quantity,category: category_id(*)),contacts: contacts!company_id(contact_id, contact_name, contact_phone)`
@@ -14,7 +13,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     .match({ company_name })
     .single();
 
-    console.log({data})
-
-  res.status(200).json(data.data);
+  res.status(status).json(data.data);
 }
