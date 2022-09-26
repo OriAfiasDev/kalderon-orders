f7ed2c640b8741727a7a95d11bb8a7cb75a2fc58
OriAfiasDev <ori@afias.net>
2022-09-26 21:39:57 +0300

    trying without env

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = 'https://jdltypannffhvympueau.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkbHR5cGFubmZmaHZ5bXB1ZWF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM1NzQzNTIsImV4cCI6MTk3OTE1MDM1Mn0.T2vjLupYrRsS9gGU4EjrBi-DOjcVMMayLcGRRf9JwWs'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
