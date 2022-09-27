0e8b06c3e8c6eac9b4db7c3f4884993d3793939e
OriAfiasDev <ori@afias.net>
2022-09-27 22:08:39 +0300

    add company api

--- utils/conversions.ts
+++ utils/conversions.ts
@@ -1,8 +1,44 @@
-export const convertNumToDay = (num: number, type: 'short' | 'long' = 'long') => {
-  const days = { 
-    short: ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ש'],
-    long: ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת']
-  }
+export const days = {
+  short: ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ש'],
+  long: ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת'],
+};
 
+export const convertNumToDay = (num: number, type: 'short' | 'long' = 'long') => {
   return days[type][num];
-}
+};
+
+export const convertDayToNum = (day: string) => {
+  return days.long.indexOf(day);
+};
+
+export const hebToEnglish = (heb: string) => {
+  const hebToEng: { [letter: string]: string } = {
+    א: 'a',
+    ב: 'b',
+    ג: 'g',
+    ד: 'd',
+    ה: 'h',
+    ו: 'v',
+    ז: 'z',
+    ח: 'ch',
+    ט: 't',
+    י: 'y',
+    כ: 'k',
+    ל: 'l',
+    מ: 'm',
+    נ: 'n',
+    ס: 's',
+    ע: 'a',
+    פ: 'p',
+    צ: 'ts',
+    ק: 'k',
+    ר: 'r',
+    ש: 'sh',
+    ת: 't',
+  };
+
+  return heb
+    .split('')
+    .map(letter => (letter in hebToEng ? hebToEng[letter] : ''))
+    .join('');
+};
