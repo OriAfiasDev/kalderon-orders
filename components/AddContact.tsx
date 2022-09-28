9d3ed64f631c0c19643f59e18c1ef16030994db4
OriAfiasDev <ori@afias.net>
2022-09-28 13:59:39 +0300

    dummy component for adding stuff

---
+++ components/AddContact.tsx
@@ -0,0 +1,8 @@
+import { Center } from '@chakra-ui/react';
+
+interface AddContactProps {
+  company_id: string;
+}
+
+//TODO:
+export const AddContact: React.FC<AddContactProps> = () => <Center height='50' bgColor='blackAlpha.100' boxShadow='lg' my='6'>Add Contact - Coming Soon</Center>;
---
+++ components/ProductsTable/AddCategory.tsx
@@ -0,0 +1,8 @@
+import { Center } from '@chakra-ui/react';
+
+interface AddCategoryProps {
+  company_id: string;
+}
+
+//TODO:
+export const AddCategory: React.FC<AddCategoryProps> = () => <Center height='50' bgColor='blackAlpha.100' boxShadow='lg' my='6'>Add Category - Coming Soon</Center>;
