a3a49a78872ff9a6d899453e3b7504729087f700
OriAfiasDev <ori@afias.net>
2022-09-28 15:31:39 +0300

    add category

--- components/ProductsTable/AddCategory.tsx
+++ components/ProductsTable/AddCategory.tsx
@@ -1,8 +1,37 @@
-import { Center } from '@chakra-ui/react';
+import { useCallback, useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import { Button, Container, Heading, Input } from '@chakra-ui/react';
+import { supabase } from '@utils/supabaseClient';
+import { ICategory } from '@types';
 
 interface AddCategoryProps {
   company_id: string;
 }
 
-//TODO:
-export const AddCategory: React.FC<AddCategoryProps> = () => <Center height='50' bgColor='blackAlpha.100' boxShadow='lg' my='6'>Add Category - Coming Soon</Center>;
+export const AddCategory: React.FC<AddCategoryProps> = ({ company_id }) => {
+  const [categoryName, setCategoryName] = useState<string>('');
+
+  const handleAdd = useCallback(async () => {
+    const product: ICategory = {
+      category_id: uuid(),
+      category_name: categoryName,
+      company_id,
+    };
+
+    await supabase.from('categories').insert(product);
+    setCategoryName('');
+  }, [categoryName, company_id]);
+
+  return (
+    <Container my='2' dir='rtl'>
+      <Heading fontSize='larger' mb='2'>
+        הוסף קטגוריה
+      </Heading>
+      <Input mb='1' placeholder='שם קטגוריה' value={categoryName} onChange={e => setCategoryName(e.target.value)} />
+
+      <Button onClick={handleAdd} w='100%'>
+        הוסף קטגוריה
+      </Button>
+    </Container>
+  );
+};
