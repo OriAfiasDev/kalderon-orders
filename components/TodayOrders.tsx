09d10043501a4c46b9ca920ce74e89de6014cd36
OriAfiasDev <ori@afias.net>
2022-09-28 21:11:23 +0300

    stack todays companies

--- components/TodayOrders.tsx
+++ components/TodayOrders.tsx
@@ -1,7 +1,7 @@
 import { ICompanySmall } from '@types';
 import { convertNumToDay } from '@utils/conversions';
 import { useMemo } from 'react';
-import { Box, Heading, Link, Text } from '@chakra-ui/react';
+import { Box, Heading, Link, Stack, Text } from '@chakra-ui/react';
 import { WarningTwoIcon } from '@chakra-ui/icons';
 import { CheckCircleIcon } from '@chakra-ui/icons';
 
@@ -15,22 +15,24 @@ export const TodayOrders: React.FC<TodayOrdersProps> = ({ todayOrders }) => {
 
   return (
     <Box textAlign='center' py={10} px={6}>
-      {todayOrders.length ? (
-        <WarningTwoIcon boxSize={'50px'} color={'orange.300'} />
-      ) : (
-        <CheckCircleIcon boxSize={'50px'} color={'green.500'} />
-      )}
-
-      <Heading as='h2' size='xl' mt={6} mb={2}>
+      <Heading as='h2' size='xl'>
         היום יום {today}
       </Heading>
 
-      <Text color={'gray.500'}>{text}</Text>
-      {todayOrders.map(({ company_name, company_id, company_name_english }) => (
-        <Link href={`/companies/${company_name_english}`} key={company_id}>
-          {company_name}
-        </Link>
-      ))}
+      {todayOrders.length ? (
+        <WarningTwoIcon boxSize={'50px'} color={'orange.300'} my={4} />
+      ) : (
+        <CheckCircleIcon boxSize={'50px'} color={'green.500'} my={4} />
+      )}
+
+      <Text fontSize='lg' color={'gray.500'} mb='2'>{text}</Text>
+      <Stack>
+        {todayOrders.map(({ company_name, company_id, company_name_english }) => (
+          <Link href={`/companies/${company_name_english}`} key={company_id}>
+            {company_name}
+          </Link>
+        ))}
+      </Stack>
     </Box>
   );
 };
