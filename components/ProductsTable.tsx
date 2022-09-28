74c11f102ed7a22cbfb570fe537440ea267628d7
OriAfiasDev <ori@afias.net>
2022-09-28 11:35:13 +0300

    added sort and minor ui changes

--- components/ProductsTable.tsx
+++ components/ProductsTable.tsx
@@ -1,7 +1,6 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import axios from 'axios';
-import Router, { useRouter } from 'next/router';
-import { Button, Center, Divider, Input, Table, Tbody, Td, Thead, Tr } from '@chakra-ui/react';
+import { Button, Center, Divider, Input, Table, TableContainer, Tbody, Td, Thead, Tr } from '@chakra-ui/react';
 import { IContact, IProduct } from '@types';
 
 interface ProductTableProps {
@@ -26,11 +25,25 @@ const getWhatsappLink = (contact: IContact, products: IProduct[]) => {
   return `https://wa.me/${contact.contact_phone}?text=${productsString}`;
 };
 
+type SortKey = 'category' | 'product_name' | 'current_quantity' | 'order_quantity';
+
 export const ProductsTable: React.FC<ProductTableProps> = ({ products, contact }) => {
-  const router = useRouter();
-  const [updatedProducts, setUpdatedProducts] = React.useState<IProduct[]>(products);
+  const [updatedProducts, setUpdatedProducts] = useState<IProduct[]>(products);
+  const [sortKey, setSortKey] = useState<SortKey>('category');
   const canOrder = useMemo(() => updatedProducts.some(product => product.order_quantity > 0), [updatedProducts]);
   const canSave = useMemo(() => updatedProducts.some(product => product.current_quantity > 0), [updatedProducts]);
+
+  const sortFunc = (a: IProduct, b: IProduct): number => {
+    if (sortKey === 'category') {
+      return b.category.category_name.localeCompare(a.category.category_name);
+    }
+
+    if (sortKey === 'product_name') {
+      return b[sortKey].localeCompare(a[sortKey]);
+    }
+
+    return b[sortKey] - a[sortKey];
+  };
 
   const handleProductUpdate = (productId: string, field: keyof IProduct, value: any) => {
     setUpdatedProducts(prev => prev.map(p => (p.product_id === productId ? { ...p, [field]: value } : p)));
@@ -55,40 +68,65 @@ export const ProductsTable: React.FC<ProductTableProps> = ({ products, contact }
 
   return (
     <>
-      <Table dir='rtl' align='center' justifyContent='center'>
-        <Thead>
-          <Tr>
-            <Td>קטגוריה</Td>
-            <Td>שם מוצר</Td>
-            <Td>מלאי</Td>
-            <Td>הזמנה</Td>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {updatedProducts.map(product => (
-            <Tr key={product.product_id}>
-              <Td>{product.category.category_name}</Td>
-              <Td>{product.product_name}</Td>
-              <Td>
-                <Input
-                  width='20'
-                  type='number'
-                  value={product.current_quantity}
-                  onChange={e => handleProductUpdate(product.product_id, 'current_quantity', Number(e.target.value))}
-                />
+      <TableContainer>
+        <Table dir='rtl' align='center' justifyContent='center' size='sm'>
+          <Thead>
+            <Tr>
+              <Td cursor='pointer' color={sortKey === 'category' ? 'cyan.500' : 'blackAlpha.500'} onClick={() => setSortKey('category')}>
+                קטגוריה
               </Td>
-              <Td>
-                <Input
-                  width='20'
-                  type='number'
-                  value={product.order_quantity}
-                  onChange={e => handleProductUpdate(product.product_id, 'order_quantity', Number(e.target.value))}
-                />
+              <Td
+                cursor='pointer'
+                color={sortKey === 'product_name' ? 'cyan.500' : 'blackAlpha.500'}
+                onClick={() => setSortKey('product_name')}>
+                שם מוצר
+              </Td>
+              <Td
+                cursor='pointer'
+                color={sortKey === 'current_quantity' ? 'cyan.500' : 'blackAlpha.500'}
+                onClick={() => setSortKey('current_quantity')}>
+                מלאי
+              </Td>
+              <Td
+                cursor='pointer'
+                color={sortKey === 'order_quantity' ? 'cyan.500' : 'blackAlpha.500'}
+                onClick={() => setSortKey('order_quantity')}>
+                הזמנה
               </Td>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            {updatedProducts.sort(sortFunc).map(product => (
+              <Tr key={product.product_id}>
+                <Td>{product.category.category_name}</Td>
+                <Td>{product.product_name}</Td>
+                <Td>
+                  <Input
+                    size='sm'
+                    width='10'
+                    variant='flushed'
+                    type='number'
+                    focusBorderColor='cyan.500'
+                    value={product.current_quantity}
+                    onChange={e => handleProductUpdate(product.product_id, 'current_quantity', Number(e.target.value))}
+                  />
+                </Td>
+                <Td>
+                  <Input
+                    size='sm'
+                    width='10'
+                    variant='flushed'
+                    type='number'
+                    focusBorderColor='cyan.500'
+                    value={product.order_quantity}
+                    onChange={e => handleProductUpdate(product.product_id, 'order_quantity', Number(e.target.value))}
+                  />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
       <Center>
         <Button onClick={() => updateQuantities(updatedProducts)} disabled={!canSave}>
           שמור מלאי
